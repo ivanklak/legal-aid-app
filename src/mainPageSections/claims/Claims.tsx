a8673d398e75e0f1bdfd8ad1f1ee9ee6c52745be
Иван Клакоцкий <ivanklak17@gmail.com>
2023-06-28 10:22:33 +0200

    добавил таблицу обращений на главную

--- src/mainPageSections/claims/Claims.tsx
+++ src/mainPageSections/claims/Claims.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect, useState} from "react";
+import React, {FC, useCallback, useEffect, useState} from "react";
 import {IAppeals} from "../mainPage/MainPage";
 import styles from "./Claims.module.css";
 import AppealsItem from "../appealsItem";
@@ -6,17 +6,46 @@ import {useNavigate} from "react-router-dom";
 import {BsArrowDownShort} from "react-icons/bs";
 import getClaimsRequest from "../api/metods/getClaimsRequest";
 import {ClaimsItemResponse} from "../api/requests/GetClaimsRequest";
+import { Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import {getDateFromString} from "../../handlers/getDateFromString";
+
+interface ClaimData {
+    key: React.Key;
+    id: string; //120
+    name: string; //"Жалоба на врача"
+    createdDate: string; // "2023-06-12 16:52:48.343"
+    status: string; //"RESOLVED"
+    // text: string; //"Колоноскопия прошла не успешно - я обосрался"
+}
 
 const Claims: FC = () => {
     const navigate = useNavigate();
     const [claims, setClaims] = useState<ClaimsItemResponse[]>([]);
+    const [rows, setRows] = useState<ClaimData[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
 
     const onAllAppealsClick = () => {
         return navigate('myRequests')
     }
 
-    const requestClaims = async () => {
+    const createTableRows = useCallback((): any => {
+        if (!claims.length) return null;
+
+        const rowsArray: ClaimData[] = claims.map((item) => {
+            return {
+                key: item.id,
+                id: item.id,
+                name: item.name,
+                createdDate: getDateFromString(item.createdDate),
+                status: item.status,
+            }
+        })
+
+        setRows(rowsArray);
+    }, [claims])
+
+    const requestClaims = useCallback(async () => {
         const sessionId = localStorage.getItem('id');
         if (!sessionId) return;
         setLoading(true);
@@ -25,6 +54,7 @@ const Claims: FC = () => {
             if (response) {
                 console.log('res', response)
                 setClaims(response.claims);
+                createTableRows();
                 setLoading(false);
             }
         } catch (err) {
@@ -36,32 +66,70 @@ const Claims: FC = () => {
         //     { headers: {'Content-Type': 'application/json'} }
         // );
         // console.log('response', response)
-
         // const response2 = await axios.post('/claims', { headers: {'Content-Type': 'application/json'} });
-    }
+    }, [createTableRows])
 
     useEffect( () => {
         requestClaims();
-    }, [])
+    }, [JSON.stringify(claims)])
 
-    console.log('claims', typeof claims)
-    if (!claims.length) return null;
+    const columns: ColumnsType<ClaimData> = [
+        {
+            title: 'Название',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Номер',
+            dataIndex: 'id',
+            width: 100,
+        },
+        {
+            title: 'Дата',
+            dataIndex: 'createdDate',
+            width: 150,
+        },
+        {
+            title: 'Статус',
+            dataIndex: 'status',
+            width: 150,
+            render: (_, {status}) => (
+                <Tag color={'geekblue'} key={status}>
+                    {status}
+                </Tag>
+            ),
+        },
+    ]
 
-    return loading ? <div>loading</div> : (
+    const handleTableChange = (val: any) => {
+        console.log('change', val)
+    }
+
+    // if (!claims.length) return null;
+
+    return (
         <>
-            <div className={styles.sortBlock}>
-                <div className={styles.sortTab}>Id обращения</div>
-                <div className={styles.sortTab}>
-                    <span className={styles.tabText}>Дата</span>
-                    <BsArrowDownShort size={16} />
-                </div>
-                <div className={styles.sortTab}>Организация</div>
-                <div className={styles.sortTab}>Статус</div>
-            </div>
+            {/*<div className={styles.sortBlock}>*/}
+            {/*    <div className={styles.sortTab}>Id обращения</div>*/}
+            {/*    <div className={styles.sortTab}>*/}
+            {/*        <span className={styles.tabText}>Дата</span>*/}
+            {/*        <BsArrowDownShort size={16} />*/}
+            {/*    </div>*/}
+            {/*    <div className={styles.sortTab}>Организация</div>*/}
+            {/*    <div className={styles.sortTab}>Статус</div>*/}
+            {/*</div>*/}
             <div className={styles.claims}>
-                {claims.map((item) => (
-                    <AppealsItem item={item} key={item.id} />
-                ))}
+                {/*{claims.map((item) => (*/}
+                {/*    <AppealsItem item={item} key={item.id} />*/}
+                {/*))}*/}
+                <Table
+                    columns={columns}
+                    dataSource={rows}
+                    loading={loading}
+                    rowKey={(record) => record.id}
+                    pagination={false}
+                    scroll={{ y: '100%' }}
+                    onChange={handleTableChange}
+                />
                 <div className={styles.allAppeals} onClick={onAllAppealsClick}>
                     <div className={styles.textButton}>
                         Все обращения
