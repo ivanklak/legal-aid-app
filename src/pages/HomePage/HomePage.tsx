b827345b5b195c3390b5ae630efbf9102984ebd7
Иван Клакоцкий <ivanklak17@gmail.com>
2023-12-25 20:18:35 +0100

    главная страница

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,9 @@
 import React, {memo, useCallback} from "react";
 import styles from "./HomePage.module.sass";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useAuth} from "../../components/hooks/useAuth";
 import NotAuthHeader from "../../components/header/notAuthHeader/NotAuthHeader";
+import {BsChevronRight} from "react-icons/bs";
 
 interface HomePageProps {}
 
@@ -42,23 +43,35 @@ const HomePage = memo<HomePageProps>(() => {
                             <div className={styles['item']}>
                                 <div className={styles['title-container']}>
                                     <div className={styles['item-title']}>Приватно</div>
-                                    <div className={styles['item-caption']}>Напрямую в организацию</div>
+                                    <div className={styles['item-caption']}>Напрямую в организацию.</div>
                                 </div>
-                                <div className={styles['item-text']}>Вы получите уведомление о статусе вашего обращения и о его решении, а так же сможете вести деловую переписку с организацией.</div>
+                                <div className={styles['item-text']}>Получайте уведомления по вашему обращению. Ведите деловую переписку с организацией.</div>
+                                <Link to={'/use-cases'} className={styles['find-out-link']}>
+                                    <span>Узнать больше</span>
+                                    <BsChevronRight className={styles['chevron-icon']} size={10} />
+                                </Link>
                             </div>
                             <div className={styles['item']}>
                                 <div className={styles['title-container']}>
                                     <div className={styles['item-title']}>Публично</div>
-                                    <div className={styles['item-caption']}>Чтобы все увидели</div>
+                                    <div className={styles['item-caption']}>Чтобы все увидели.</div>
                                 </div>
                                 <div className={styles['item-text']}>Все пользователи смогут видеть, поддерживать или дополнять ваше обращение.</div>
+                                <Link to={'/use-cases'} className={styles['find-out-link']}>
+                                    <span>Узнать больше</span>
+                                    <BsChevronRight className={styles['chevron-icon']} size={10} />
+                                </Link>
                             </div>
                             <div className={styles['item']}>
                                 <div className={styles['title-container']}>
                                     <div className={styles['item-title']}>Куда следует</div>
-                                    <div className={styles['item-caption']}>Куда следует</div>
+                                    <div className={styles['item-caption']}>Если не знаете куда.</div>
                                 </div>
-                                <div className={styles['item-text']}>Направьте свою жалобу в компетентные органы.</div>
+                                <div className={styles['item-text']}>Направьте свою жалобу в компетентные органы. Мы сами оформим все официально.</div>
+                                <Link to={'/use-cases'} className={styles['find-out-link']}>
+                                    <span>Узнать больше</span>
+                                    <BsChevronRight className={styles['chevron-icon']} size={10} />
+                                </Link>
                             </div>
                         </div>
                     </div>
