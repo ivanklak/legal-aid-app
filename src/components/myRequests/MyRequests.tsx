fb13462dac316962aa154b07ff66459c13ea7367
Иван Клакоцкий <ivanklak17@gmail.com>
2024-09-11 17:01:31 +0200

    Добавил получение обращений на странице Мои обращения

--- src/components/myRequests/MyRequests.tsx
+++ src/components/myRequests/MyRequests.tsx
@@ -2,65 +2,128 @@ import React, {useEffect, useState} from "react";
 import MainWrapper from "../mainWrapper/MainWrapper";
 import styles from "./MyRequests.module.sass";
 import {LoaderCircle} from "../loader/Loader.Circle";
-import {UploadFile} from "antd";
 import {ISuggestions} from "../../newRequest/api/requests/GetOrganisationSuggestionsRequest";
-
-interface DraftData {
-    name?: string;
-    text?: string;
-    files?: UploadFile[];
-    orgData?: ISuggestions;
-    orgName?: string;
-    orgInn?: string;
-    orgAddress?: string;
-}
-
-const DRAFT_ITEMS = ['name', 'text', 'files', 'orgData', 'orgName', 'orgInn', 'orgAddress'];
+import {IFullRequestInfo} from "../../newRequest/newRequestForm/parts/newRequestFinalPart/NewRequestFinalPart";
+import {formats} from "../../requestItem/textEditor/EditorToolbar";
+import ReactQuill from "react-quill";
+import {IOrganisationData} from "../../newRequest/NewRequestDataLayer";
+import {useNavigate} from "react-router-dom";
 
 const MyRequests = () => {
+    const navigate = useNavigate();
     const [isReady, setIsReady] = useState<boolean>(false);
-    const [draft, setDraft] = useState<DraftData>(null);
+    const [requests, setRequests] = useState<IFullRequestInfo[]>([]);
 
     useEffect(() => {
-        const draftObj: Record<string, any> = {} as DraftData;
-        DRAFT_ITEMS.forEach((item => {
-            const draftItem = getDraftData(item);
-            if (!!draftItem) {
-                draftObj[item] = draftItem;
-            }
-        }))
-        setDraft(draftObj);
-        //
-        setIsReady(true);
+        getAllClaims()
+            .then((data: IFullRequestInfo[]) => {
+                if (!data?.length) {
+                    setIsReady(true);
+                    setRequests([]);
+                    return;
+                }
+                setRequests(data);
+                setIsReady(true);
+            })
+            .catch((e) => {
+                console.log('нет обращений', e);
+                setIsReady(true);
+            })
     }, [])
 
-    const getDraftData = (name: string) => {
-        const item = localStorage.getItem(`claim.draft.${name}`);
-        if (!item) return null;
-        try {
-            return JSON.parse(item);
-        } catch {
-            return null;
-        }
+    const getAllClaims = (): Promise<IFullRequestInfo[]> => {
+        return new Promise((resolve, reject) => {
+            const existedRequestsString = localStorage.getItem('user_requests');
+            let existedRequestsArray: IFullRequestInfo[] = [];
+            try {
+                if (existedRequestsString) {
+                    const parsedRegUsers: IFullRequestInfo[] = JSON.parse(existedRequestsString) || [];
+
+                    if (parsedRegUsers?.length) {
+                        existedRequestsArray.push(...parsedRegUsers);
+                    }
+                }
+            } catch (e) {
+                console.error('Cannot parse user_requests in MyRequests -> existedRequestsString', existedRequestsString);
+            }
+
+            if (!existedRequestsArray.length) {
+                reject();
+                return;
+            }
+
+            window.setTimeout(() => {
+                resolve(existedRequestsArray);
+            }, 1000)
+        })
     }
 
-    const renderDraftItem = () => {
-
-        if (!draft || !draft.name || !draft.text || !draft.orgData) return null
-
+    const renderOrg = (name: string, address: string, inn: string) => {
         return (
-            <div className={styles['draft']}>
-                <span className={styles['caption']}>Черновик</span>
-                {draft.name && <div>Название: {draft.name}</div>}
-                {draft.orgData && <div>Организация: {draft.orgData.value}</div>}
-                {draft.text && (
-                    <div>
-                        <span>Описание:</span>
-                        <div className={styles['text']} dangerouslySetInnerHTML={{__html: draft.text}} />
-                    </div>
-                )}
+            <div className={styles['org-table']}>
+                <div className={styles['org-row']}>
+                    <div className={styles['key']}>Название</div>
+                    <div className={styles['value']}>{name}</div>
+                </div>
+                <div className={styles['org-row']}>
+                    <div className={styles['key']}>Адрес</div>
+                    <div className={styles['value']}>{address}</div>
+                </div>
+                <div className={styles['org-row']}>
+                    <div className={styles['key']}>Инн</div>
+                    <div className={styles['value']}>{inn}</div>
+                </div>
             </div>
         )
+    }
+
+    const handleRequestClick = (id: string) => {
+        navigate(`/myRequests/${id}`)
+    }
+
+    const isSuggestion = (info: IOrganisationData): info is ISuggestions => {
+        return info && Boolean((info as ISuggestions).data) && Boolean((info as ISuggestions).value)
+    }
+
+    const renderRequests = (): JSX.Element => {
+        return (
+            <>
+                {requests.map((requestItem: IFullRequestInfo, index) => {
+                    const statusMessage = requestItem.status === 'created' ? 'Создано' : 'Создано';
+
+                    const firstIndex = requestItem.requestText.indexOf('<p>');
+                    const lastIndex = requestItem.requestText.indexOf('</p>');
+                    const shortDescription = requestItem.requestText.slice(firstIndex, lastIndex + 4);
+
+                    return (
+                        <div key={`${requestItem.id}_${index}`} className={styles['request']}>
+                            {requestItem.status && <div className={styles['status']}>{statusMessage}</div>}
+                            <div
+                                className={styles['caption']}
+                                onClick={() => handleRequestClick(requestItem.id)}
+                            >
+                                {requestItem.reason.text}
+                            </div>
+                            <div className={styles['name']}>Куда</div>
+                            {isSuggestion(requestItem.org)
+                                ? renderOrg(requestItem.org.value, requestItem.org.data.address.value, requestItem.org.data.inn)
+                                : renderOrg(requestItem.org.name, requestItem.org.address, requestItem.org.inn)
+                            }
+                            <div className={styles['name']}>Описание</div>
+                            <ReactQuill
+                                className={styles['text']}
+                                theme="snow"
+                                value={shortDescription || '...'}
+                                formats={formats}
+                                modules={{toolbar: null}}
+                                readOnly={true}
+                            />
+                            <div className={styles['comments']}>Комментарии: {requestItem.comments?.length ? requestItem.comments.length : 0}</div>
+                        </div>
+                    )
+                })}
+            </>
+        );
     }
 
     return !isReady ? <LoaderCircle /> : (
@@ -68,8 +131,11 @@ const MyRequests = () => {
             <div className={styles['my-requests']}>
                 <div className={styles['scroll-area']}>
                     <div className={styles['title']}>Мои обращения</div>
-                    <div className={styles['drafts-container']}>
-                        {renderDraftItem()}
+                    <div className={styles['requests-container']}>
+                        {!!requests.length
+                            ? renderRequests()
+                            : <div>У вас пока что нет обращений</div>
+                        }
                     </div>
                 </div>
             </div>
